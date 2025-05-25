b277c3356877033d468b56fc1b92702f7b24db09
Miguel Reyes <miguelvalentino2403@gmail.com>
2025-05-24 20:26:37 -0400

    Modificado InscripcionService

--- src/main/java/com/vcore/backend_plataforma_web/service/InscripcionService.java
+++ src/main/java/com/vcore/backend_plataforma_web/service/InscripcionService.java
@@ -1,5 +1,24 @@
 package com.vcore.backend_plataforma_web.service;
 
-public class InscripcionService {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.vcore.backend_plataforma_web.model.Inscripcion;
+import com.vcore.backend_plataforma_web.repository.InscripcionRepository;
+
+@Service
+public class InscripcionService {
+    @Autowired
+    private InscripcionRepository inscripcionRepository;
+
+    public String almacenar(Inscripcion inscripcion) {
+            inscripcionRepository.save(inscripcion);
+        return "Inscripcion ingresada";
+    }
+
+    public List<Inscripcion> listar() {
+        return inscripcionRepository.findAll();
+    }
 }
